62514f668fdb4aab67edf43c55d07a6465b0418d
Sharath <popuri.sharath@gmail.com>
2025-04-10 23:13:54 +0530

    changed the url

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ import random
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=['https://chess-4u2b.onrender.com'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
